694983befe95f2c1d16e887af2d08498ec163303
Danster4 <86613317+Danster4@users.noreply.github.com>
2022-03-28 21:11:38 -0400

    started nav bar and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,7 @@ function App() {
        
 
         <Button>I am a Bootstrap Button!</Button>
+        <Footer />
       
     </div>
   );
