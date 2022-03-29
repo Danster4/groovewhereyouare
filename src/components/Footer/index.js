946ab8411847a33bed4998858f9286707676eead
Danster4 <86613317+Danster4@users.noreply.github.com>
2022-03-29 10:54:29 -0400

    started footer fixed to bottom

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import logo from '../../logo.svg';
+
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2022 by Danny Urkov</div>
-    </footer>
+    <Navbar fixed="bottom" bg="dark" variant="dark" className="justify-content-between" expand={false}>
+      <Container fluid>
+        <Navbar.Brand href="#home">
+          <img
+            alt="logo"
+            src={logo}
+            width="30"
+            height="30"
+            className="justify-content-between"
+          />{' '}
+            Groove Where You Are
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   )
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -11,35 +11,34 @@ import logo from '../../logo.svg';
 
 const Nav = () => {
   return (
-<Navbar bg="dark" variant="dark" expand={false}>
-  <Container fluid>
-  <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src={logo}
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-      Groove Where You Are
-      </Navbar.Brand>
-    <Navbar.Toggle aria-controls="offcanvasNavbar" />
-    <Navbar.Offcanvas
+    <Navbar bg="dark" variant="dark" expand={false}>
+      <Container fluid>
+        <Navbar.Brand href="#home">
+          <img
+            alt="logo"
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+            Groove Where You Are
+          </Navbar.Brand>
+      
+      <Navbar.Toggle aria-controls="offcanvasNavbar" />
+      <Navbar.Offcanvas
       id="offcanvasNavbar"
-      bg="dark"
-      variant="dark"
       aria-labelledby="offcanvasNavbarLabel"
       placement="end"
     >
       <Offcanvas.Header closeButton>
-        <Offcanvas.Title id="offcanvasNavbarLabel">Groove Where You Are</Offcanvas.Title>
+        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
         <Nav className="justify-content-end flex-grow-1 pe-3">
           <Nav.Link href="#action1">Home</Nav.Link>
           <Nav.Link href="#action2">Link</Nav.Link>
           <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-            <NavDropdown.Item href="#action3">Cities</NavDropdown.Item>
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action5">
@@ -58,8 +57,8 @@ const Nav = () => {
         </Form>
       </Offcanvas.Body>
     </Navbar.Offcanvas>
-  </Container>
-</Navbar>
+    </Container>
+  </Navbar>
 
 
   )
